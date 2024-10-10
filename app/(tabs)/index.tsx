6d9466de0be35ce787f0db6aafb45f294cb8cd2e
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-10-10 19:21:45 +0200

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,11 @@ import { useAuth } from '@/providers/AuthProvider';
 
 export default function HomeScreen() {
   const { user } = useAuth();
-  const fullname = user?.first_name + ' ' + user?.last_name;
-  console.log(fullname);
+  
   return (
     <View className="flex-1 items-center justify-center bg-white" >
       
-      <Text className="font-bold text-2xl text-black" >{fullname}</Text>
+      <Text className="font-bold text-2xl text-black" >{user.first_name}</Text>
     </View>
   );
 }
