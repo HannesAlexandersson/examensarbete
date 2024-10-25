1ead7fc8629b059699e7af127918560bdeb168b5
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-10-25 12:26:40 +0200

    FIXED album bugg
    by using flatlist and solve the memoryleaks

--- components/VideoThumbnails.tsx
+++ components/VideoThumbnails.tsx
@@ -6,9 +6,10 @@ export default function VideoThumbnail({videoUri}: {videoUri: string}) {
   const [thumbnail, setThumbnail] = useState<string | null>(null);
 
   useEffect(() => {
+    console.log('videoUri');
     const getThumbnail = async () => {
       const { uri } = await VideoThumbnails.getThumbnailAsync(videoUri, {
-        time: 15000,
+        time: 1000,
       });
       setThumbnail(uri);
     }
@@ -16,15 +17,15 @@ export default function VideoThumbnail({videoUri}: {videoUri: string}) {
     getThumbnail();
   }, []);
 
+  const MemoizedImage = React.memo(({ uri }: { uri: string } ) => (
+    <Image source={{ uri }} style={{ width: 128, height: 128, resizeMode: 'cover' }} />
+  ));
   return(    
-      <View >
+      <>
         {thumbnail && (
-          <Image
-            source={{ uri: thumbnail }}
-            className='w-32 h-32'
-          />
+          <MemoizedImage uri={thumbnail} />
         )}
-      </View>
+      </>
   )
 }
     
